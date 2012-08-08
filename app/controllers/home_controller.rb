fc6c264f360bf0613ee476f9b35c29135098ca47
vince <vincent@djengo.net>
2012-08-08 16:59:13 +0200

    modif tardive

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,18 +7,18 @@ class HomeController < ApplicationController
 
     @vocabulary = YmlLoader.load_yaml("fr.yml")
 
-    @liste = TranslationsManager.hash_to_export(@vocabulary, "", "-", "<br>")
-
-    @liste_without_key = @liste.gsub(/[-]{8}.+?<br>/, "")
-
-
-    @liste_v2 = TranslationsManager.export_liste(@vocabulary, "", "li", "")
-
-    @liste_without_key_2 = @liste_v2
-
     hash_path = ""
     indent = ""
     @count = YmlLoader.parse_yaml_toeic_to_db(@vocabulary, hash_path, indent)
+
+
+    @liste = TranslationsManager.hash_to_export(@vocabulary, "", "-", "<br>")
+    @liste_without_key = @liste.gsub(/[-]{8}.+?<br>/, "")
+
+    @liste_v2 = TranslationsManager.export_liste(@vocabulary, "", "li", "")
+    @liste_without_key_2 = @liste_v2
+
+
 
     # @printed_hash = TranslationsManager.hash_to_export(@vocabulary, "", "&nbsp;", "<br />")
   end
